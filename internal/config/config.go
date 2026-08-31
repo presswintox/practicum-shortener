8335dd816a337dc937fdd7f1423c9c0b091e7572
Дмитрий Бикмухаметов <bikmuhametov@foxford.ru>
2026-08-31 21:02:06 +0300

    fix NewConfig

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,7 +7,7 @@ type Config struct {
 	ShortUrlAddr string
 }
 
-func NewConfig(port, url string) *Config {
+func NewConfig() *Config {
 	cfg := &Config{}
 	flag.StringVar(&cfg.Port, "a", ":8080", "address and port to run server")
 	flag.StringVar(&cfg.ShortUrlAddr, "b", "http://localhost:8080", "address and port for short url")
